a8510eaa49e5031a71b0a1b76eb5c54619dec225
Roman Tkachuk <tkachukrr@gmail.com>
2023-07-18 00:48:33 +0200

    001 created siple template with styles and serch form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-search',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  private _fb = inject(FormBuilder);
+
+  public searchForm = this._fb.nonNullable.group({
+    value: this._fb.nonNullable.control<string>(''),
+    date: this._fb.nonNullable.control<string>('')
+  })
+
+  public search(): void {
+    this.searchForm.controls.date.patchValue(Date.now().toString())
+  }
 }
